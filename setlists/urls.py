3eb22875eb2e7c7c323cdb7e3c08ba1c33bf9e2f
jls83 <josephlsanders@gmail.com>
2016-01-13 18:13:20 -0500

    Moved last_seen into class, edited templates
    Implemented song_not_seen in the SongDetailView class, placed relevant
    data in template

--- setlists/urls.py
+++ setlists/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 app_name = 'setlists'
 urlpatterns = [
-    url(r'^$', views.ShowIndexView.as_view(), name='index'),
+    url(r'^$', views.RecentShowView.as_view(), name='index'),
     #Show URLs
-    url(r'^shows/$', views.RecentShowView.as_view(), name='show_index'),
+    url(r'^shows/$', views.ShowIndexView.as_view(), name='show_index'),
     url(r'^shows/(?P<s_d>(\d{4}-\d{2}-\d{2}))/$', views.ShowDetailView.as_view(), name='show_detail'),
     #Song URLs
     url(r'^songs/$', views.SongIndexView.as_view(), name='song_index'),
     url(r'^songs/(?P<title>[\w-]+)/$', views.SongDetailView.as_view(), name='song_detail'),
-    url(r'^notseen/(?P<title>[\w-]+)/$', views.song_not_seen, name='song_not_seen'),
+#    url(r'^notseen/(?P<title>[\w-]+)/$', views.song_not_seen, name='song_not_seen'),
 ]
--- setlists/views.py
+++ setlists/views.py
@@ -3,15 +3,30 @@ from django.views import generic
 from .models import *
 
 class ShowIndexView(generic.ListView):
-    template_name = 'setlists/index.html'
+    template_name = 'setlists/show_index.html'
     context_object_name = 'show_list'
     list_obj = Show.objects.order_by('-show_date')
+    date_list = [ i.show_date for i in list_obj ]
 
     def get_queryset(self):
         return self.list_obj
 
 class RecentShowView(ShowIndexView):
+    template_name = 'setlists/index.html'
     list_obj = Show.objects.order_by('-show_date')[:5]
+
+class ShowDetailView(generic.ListView):
+    model = Show
+    template_name = 'setlists/show_detail.html'
+    context_object_name = 'show_obj'
+    slug_field = 'show_date'
+    slug_url_kwarg = 's_d'
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowDetailView, self).get_context_data(**kwargs)
+        input_q = self.kwargs['s_d']
+        context['mas'] = ShowRelation.objects.filter(show__show_date=input_q).order_by('track_position')
+        return context
 
 class SongIndexView(generic.ListView):
     template_name = 'setlists/song_index.html'
@@ -28,37 +43,35 @@ class SongDetailView(generic.DetailView):
     slug_field = 'simple_title'
     slug_url_kwarg = 'title'
 
+    def song_not_seen(self, title):
+        s_l = ShowIndexView.date_list
+        if ShowRelation.objects.filter(song__simple_title=title):
+            l_version = ShowRelation.objects.filter(song__simple_title=title).latest('show__show_date')
+            lv_date = l_version.show.show_date
+            last_seen = s_l.index(lv_date)
+        else:
+            l_version = []
+            last_seen = []
+        return (l_version, last_seen)
+
     def get_context_data(self, **kwargs):
-        context = super(SongDetailView, self).get_context_data(**kwargs)
         input_simple_title = self.kwargs['title']
+        context = super(SongDetailView, self).get_context_data(**kwargs)
         context['played_list'] = get_list_or_404(ShowRelation, song__simple_title=input_simple_title)
+        context['last_seen_info'] = self.song_not_seen(input_simple_title)
         return context
-
-class ShowDetailView(generic.ListView):
-    model = Show
-    template_name = 'setlists/show_detail.html'
-    context_object_name = 'show_obj'
-    slug_field = 'show_date'
-    slug_url_kwarg = 's_d'
-
-    def get_context_data(self, **kwargs):
-        context = super(ShowDetailView, self).get_context_data(**kwargs)
-        input_q = self.kwargs['s_d']
-        context['mas'] = ShowRelation.objects.filter(show__show_date=input_q).order_by('track_position')
-        return context
-
 ##############################################################
-def song_not_seen(request, title):
-    show_list = Show.objects.all()
-    show_list_date = [ i.show_date for i in show_list ]
-    show_list_date.sort(reverse=True)
-
-    s = Song.objects.get(simple_title=title)
-    if ShowRelation.objects.filter(song=s):
-        song_l_version = ShowRelation.objects.filter(song=s).latest('show__show_date')
-        song_lv_date = song_l_version.show.show_date
-        song_not_seen = show_list_date.index(song_lv_date)
-    else:
-        song_not_seen = []
-        song_l_version = []
-    return render(request, 'setlists/song_not_seen.html', {'song_l_version': song_l_version, 'song_not_seen': song_not_seen, 's': s})
+#def song_not_seen(request, title):
+#    show_list = Show.objects.all()
+#    show_list_date = [ i.show_date for i in show_list ]
+#    show_list_date.sort(reverse=True)
+#
+#    s = Song.objects.get(simple_title=title)
+#    if ShowRelation.objects.filter(song=s):
+#        song_l_version = ShowRelation.objects.filter(song=s).latest('show__show_date')
+#        song_lv_date = song_l_version.show.show_date
+#        song_not_seen = show_list_date.index(song_lv_date)
+#    else:
+#        song_not_seen = []
+#        song_l_version = []
+#    return render(request, 'setlists/song_not_seen.html', {'song_l_version': song_l_version, 'song_not_seen': song_not_seen, 's': s})
