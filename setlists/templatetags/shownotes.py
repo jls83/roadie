7e5c4be68bd79a1122dacbec44d8284c4584ebd1
jls83 <josephlsanders@gmail.com>
2016-01-19 23:40:00 -0500

    Added show_notes list to show_detail pages
    Wrote another custom template filter (listify) for this purpose

--- setlists/templatetags/shownotes.py
+++ setlists/templatetags/shownotes.py
@@ -6,3 +6,9 @@ register = template.Library()
 def numberize(note_dict, simple):
     if simple in note_dict:
         return note_dict[simple][0]
+
+@register.filter(name="listify")
+def listify(note_dict):
+    note_idx = sorted(note_dict, key=note_dict.get)
+    note_list = [ note_dict[i] for i in note_idx ]
+    return note_list
